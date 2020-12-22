343ad4f8ed1679c6946e4626511d601f698a2fad
hackerhgl <hackerhgl@gmail.com>
2020-12-22 12:00:53 +0500

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -25,50 +25,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: DefaultTabController(
-      length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Collapsing Toolbar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(
+                    "Collapsing Toolbar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
                   background: Image.network(
                     "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
                     fit: BoxFit.cover,
-                  )),
-            ),
-            SliverPadding(
-              padding: new EdgeInsets.all(16.0),
-              sliver: new SliverList(
-                delegate: new SliverChildListDelegate([
-                  TabBar(
-                    labelColor: Colors.black87,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      new Tab(icon: new Icon(Icons.info), text: "Tab 1"),
-                      new Tab(
-                          icon: new Icon(Icons.lightbulb_outline),
-                          text: "Tab 2"),
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.all(16.0),
+                sliver: SliverList(
+                  delegate: new SliverChildListDelegate(
+                    [
+                      TabBar(
+                        labelColor: Colors.black87,
+                        unselectedLabelColor: Colors.grey,
+                        tabs: [
+                          Tab(icon: Icon(Icons.info), text: "Tab 1"),
+                          Tab(
+                              icon: Icon(Icons.lightbulb_outline),
+                              text: "Tab 2"),
+                        ],
+                      ),
                     ],
                   ),
-                ]),
+                ),
               ),
-            ),
-          ];
-        },
-        body: Center(
-          child: Text("Sample text"),
+            ];
+          },
+          body: Center(
+            child: Text("Sample text"),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
